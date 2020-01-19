458d62c797c82fdf10e8c1a51b169f2155b4bec7
Ayato French <ayato.french@gmail.com>
2020-01-18 22:56:50 -0800

    Added base structure to project
    Added Bulma and boilerplate files to project

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+import NProgress from 'nprogress';
+import Router, { withRouter } from 'next/router';
+import Layout from 'components/Layout';
+import 'styles/styles.scss';
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
+class MyApp extends App {
+  static async getInitialProps(props) {
+    const { Component, ctx } = props;
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx });
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Head>
+          <title>UCI | Research Discovery App</title>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+    );
+  }
+}
+
+export default withRouter(MyApp);
